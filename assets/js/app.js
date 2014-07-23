862dba86e596a1576d87223e7b66249c853610f2
hoosin@yeah.net <hoosin@yeah.net>
2014-07-23 09:58:48 +0800

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,37 +28,34 @@ function project($scope) {
 		 */ 
 
 	$scope.items1=[
-		{author:authors[0],time:'(7.17-21(3))',pageName:'首页',demo:'http://www.qq.com/',pass:'未确认',online:'',statu:status[1]},
-		{author:authors[1],time:'(7.18-21(2))',pageName:'页面',demo:'',pass:'未确认',online:'',statu:status[0]},
-		{author:authors[2],time:'(7.18-21(2))',pageName:'页面',demo:'',pass:'未确认',online:'',statu:status[2]},
-		{author:authors[0],time:'(7.22-22(1)) ',pageName:'页面',demo:'',pass:'未确认',online:'',statu:status[1]},
-		{author:authors[0],time:'(7.23-23(1))',pageName:'页面',demo:'',pass:'未确认',online:'',statu:status[0]}
+		{author:authors[0],time:'(7.17-21(3))',pageName:'首页',demo:'http://www.qq.com/',online:'',statu:status[1]},
+		{author:authors[1],time:'(7.18-21(2))',pageName:'页面',demo:'',online:'',statu:status[0]},
+		{author:authors[2],time:'(7.18-21(2))',pageName:'页面',demo:'',online:'',statu:status[2]},
+		{author:authors[0],time:'(7.22-22(1)) ',pageName:'页面',demo:'',online:'',statu:status[1]},
+		{author:authors[0],time:'(7.23-23(1))',pageName:'页面',demo:'',online:'',statu:status[0]}
 	];
 	$scope.items2=[
-		{author:authors[2],time:'(7.24-24(1))',pageName:'页面',demo:'',pass:'未确认',online:'',statu:status[3]},
-		{author:authors[2],time:'(7.25-29(5))',pageName:'页面',demo:'',pass:'未确认',online:'',statu:status[0]},
-		{author:authors[2],time:'(7.28-29(2))',pageName:'首页',demo:'http://www.qq.com/',pass:'未确认',online:'http://www.qq.com/',statu:status[2]},
-		{author:authors[2],time:'(7.28-29(2))',pageName:'页面',demo:'',pass:'未确认',online:'',statu:status[0]},
-		{author:authors[2],time:'(7.30-31(2))',pageName:'页面',demo:'',pass:'未确认',online:'',statu:status[1]},
-		{author:authors[2],time:'(7.30-31(2))',pageName:'页面',demo:'',pass:'未确认',online:'',statu:status[0]},
-		{author:authors[2],time:'(8.1-1(1))',pageName:'页面',demo:'',pass:'未确认',online:'',statu:status[0]}
+		{author:authors[2],time:'(7.24-24(1))',pageName:'页面',demo:'',online:'',statu:status[3]},
+		{author:authors[2],time:'(7.25-29(5))',pageName:'页面',demo:'',online:'',statu:status[0]},
+		{author:authors[2],time:'(7.28-29(2))',pageName:'首页',demo:'http://www.qq.com/',online:'http://www.qq.com/',statu:status[2]},
+		{author:authors[2],time:'(7.28-29(2))',pageName:'页面',demo:'',online:'',statu:status[0]},
+		{author:authors[2],time:'(7.30-31(2))',pageName:'页面',demo:'',online:'',statu:status[1]},
+		{author:authors[2],time:'(7.30-31(2))',pageName:'页面',demo:'',online:'',statu:status[0]},
+		{author:authors[2],time:'(8.1-1(1))',pageName:'页面',demo:'',online:'',statu:status[0]}
 	];
 	$scope.items3=[
-		{author:authors[2],time:'(8.1-1(1))',pageName:'页面',demo:'',pass:'未确认',online:'',statu:status[0]},
-		{author:authors[2],time:'(8.5-6(1))',pageName:'页面',demo:'',pass:'未确认',online:'',statu:status[0]},
-		{author:authors[2],time:'(8.5-5(1))',pageName:'页面',demo:'',pass:'未确认',online:'',statu:status[3]},
-		{author:authors[2],time:'(8.5-5(1))',pageName:'页面',demo:'',pass:'未确认',online:'',statu:status[0]},
-		{author:authors[2],time:'(8.5-5(1))',pageName:'页面',demo:'',pass:'未确认',online:'',statu:status[0]},
-		{author:authors[2],time:'(8.5-5(1))',pageName:'页面',demo:'',pass:'未确认',online:'',statu:status[2]},
-		{author:authors[2],time:'(8.5-5(1))',pageName:'页面',demo:'',pass:'未确认',online:'',statu:status[0]},
-		{author:authors[2],time:'(8.5-5(1))',pageName:'页面',demo:'',pass:'未确认',online:'',statu:status[0]},
-		{author:authors[2],time:'(8.5-5(1))',pageName:'页面',demo:'',pass:'未确认',online:'',statu:status[0]},
-		{author:authors[2],time:'(8.5-5(1))',pageName:'页面',demo:'',pass:'未确认',online:'',statu:status[0]},
-		{author:authors[2],time:'(8.5-5(1))',pageName:'页面',demo:'',pass:'未确认',online:'',statu:status[0]}
-	];
-
-	if (true) {};
-	
+		{author:authors[2],time:'(8.1-1(1))',pageName:'页面',demo:'',online:'',statu:status[0]},
+		{author:authors[2],time:'(8.5-6(1))',pageName:'页面',demo:'',online:'',statu:status[0]},
+		{author:authors[2],time:'(8.5-5(1))',pageName:'页面',demo:'',online:'',statu:status[3]},
+		{author:authors[2],time:'(8.5-5(1))',pageName:'页面',demo:'',online:'',statu:status[0]},
+		{author:authors[2],time:'(8.5-5(1))',pageName:'页面',demo:'',online:'',statu:status[0]},
+		{author:authors[2],time:'(8.5-5(1))',pageName:'页面',demo:'',online:'',statu:status[2]},
+		{author:authors[2],time:'(8.5-5(1))',pageName:'页面',demo:'',online:'',statu:status[0]},
+		{author:authors[2],time:'(8.5-5(1))',pageName:'页面',demo:'',online:'',statu:status[0]},
+		{author:authors[2],time:'(8.5-5(1))',pageName:'页面',demo:'',online:'',statu:status[0]},
+		{author:authors[2],time:'(8.5-5(1))',pageName:'页面',demo:'',online:'',statu:status[0]},
+		{author:authors[2],time:'(8.5-5(1))',pageName:'页面',demo:'',online:'',statu:status[0]}
+	];	
 }
 
 // 当链接为空，隐藏按钮
